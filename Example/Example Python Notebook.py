6859ba7659c18b3c1789ef2dc784459e3e057546
rhernandezcr <rhernandezcr>
2023-11-24 18:13:46 +0000

    Adding a first example notebook

---
+++ Example/Example Python Notebook.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Example notebook
+# MAGIC
+# MAGIC This is an example notebook that was originally created in the **dev** branch
+
+# COMMAND ----------
+
+print(f"This is a notebook on the dev branch")
